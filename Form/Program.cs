9794fe7c23b865456418ab55c517c2fca3aa0a17
shinkmrkan10 <shinkmrkan10@gmail.com>
2020-10-10 12:50:26 +0900

    Wraplauncher

--- Form/Program.cs
+++ Form/Program.cs
@@ -3,9 +3,9 @@ using System.Drawing;
 using System.Windows.Forms;
 
 // C:\Users\kimura\git\cs_old\cs\Form>dotnet new wpf --name Wraplauncher
-// C:\Users\kimura\git\cs_old\cs\Form>cd ./Wraplauncher
+// C:\Users\kimura\git\cs_old\cs\Form>cd ./Wraplauncher   必要
 // C:\Users\kimura\git\cs_old\cs\Form\Wraplauncher>dotnet run -c Release
-// C:\Users\kimura\git\cs_old\cs\Form\Wraplauncher>dotnet run
+// C:\Users\kimura\git\cs_old\cs\Form\Wraplauncher>dotnet run　必要
 
 namespace Form1
 {
